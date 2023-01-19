a1bf71fadfe524efaa89cad5a6e614954f4819de
Frang84 <91824293+Frang84@users.noreply.github.com>
2023-01-20 00:10:22 +0100

    Add files via upload

---
+++ wypelnainie tabel.sql
@@ -0,0 +1,89 @@
+/*INSERT INTO gatunki (nazwa)
+VALUES('Kot')*/
+
+/*SELECT * FROM gatunki */
+
+/*INSERT INTO rasa ([ID gatunku],[nazwa rasy])
+VALUES(2,'Dachowiec')*/
+
+/*SELECT * FROM rasa*/
+
+
+/*INSERT INTO Zwierzeta
+VALUES('Biala',2,1,'Lotos', '2015-10-10',5,2,'W','T','chudy kto stal sie gruby',null)*/
+
+/*select * from Zwierzeta*/
+
+/*INSERT INTO gatunki (nazwa)
+VALUES ('pies'),
+		('swinka morska')*/
+
+
+/*INSERT INTO Statusy([nazwa statusu])
+VALUES ('do adopcji'),
+		('dom tymczasowy'),
+		('w trakcie adopcji'),
+		('pilne'),
+		('wkrotce do adopcji'),
+		('w trakcie leczenia'),
+		('do adopcji z domu tymczasowego')*/
+
+
+/*INSERT INTO karmy(nazwa,ilosc,Cena,dostawca)
+VALUES  ('Favorite', 100, 4.19,'Karmex'),
+		('CEBICAN DAILY CARE DLA PSÓW DOROS£YCH', 400,6.44,'Karmex'),
+		('SUCHA KARMA JOSERA JOSIDOG ADULT ECONOMY 15', 1500,7.70,'Karmex'),
+		('KARMA MORANDO PRO PUPPY DLA SZCZENI¥T KURCZAK',324, 8.21, 'POL-KARM'),
+		('Wolf of Wilderness „The Taste Of"',1250,25.75,'POL-KARM'),
+		('Purizon Adult',305,22.38,'POL-KARM'),
+		('Purizon Adult dla kota, ryba',2000,44.50,'Karmex'),
+		('Purizon Kitten, kurczak i ryba',345,65.33,'Karmex'),
+		('Korzystny pakiet Feringa Classic Meat Menu',543,26.17,'POL-KARM'),
+		('Feringa karma mokra dla koci¹t',120,29.0,'POL-KARM')*/
+
+/*SELECT * FROM karmy*/
+	
+
+/*INSERT INTO rasa([ID gatunku],[nazwa rasy])
+VALUES (1,'Dachowiec'),
+		(1,'Brytyjski'),
+		(1,'Maine Coon'),
+		(1,'Perski'),
+		(1,'Norweski lesny'),
+		(1,'Ragdol'),
+		(2,'Mieszaniec'),
+		(2,'Owczarek Niemiecki'),
+		(2,'Amstaff'),
+		(2,'Syberian Husky'),
+		(2,'Sznaucer miniaturowy'),
+		(2,'Owczarek Belgijski'),
+		(2,'Maltanczyk'),
+		(2,'Golden Retriever'),
+		(2,'Labrador'),
+		(2,'Pinczer'),
+		(2,'Bokser'),
+		(2,'Jamnik')*/
+
+/*INSERT INTO zatrudniony (adres,telefon,wiek,imie,nazwisko,pesel)
+VALUES
+(NULL, 912800 ,61 ,' PIOTR ',' ADAMKIEWICZ ', 486607022741 ),
+(NULL, 533097 ,65 ,' IZA ',' ADAMKIEWICZ ', 556369137065 ),
+(NULL, 380254 ,59 ,' MICHA£ ',' LISOWSKI ', 424731195791 ),
+(NULL, 586668 ,51 ,' MARIANNA ',' LISOWSKI ', 556237930897 ),
+(NULL, 610869 ,63 ,' ZOFIA ',' SKORNIAK ', 251754994439 ),
+(NULL, 106429 ,44 ,' MARIANNA ',' OPALA ', 932495398960 ),
+(NULL, 486931 ,21 ,' MICHA£ ',' ADAMKIEWICZ ', 224652302247 ),
+(NULL, 579772 ,28 ,' IZA ',' SKORNIAK ', 816274571503 ),
+(NULL, 976282 ,25 ,' WIKTORIA ',' ADAMKIEWICZ ', 839196394433 ),
+(NULL, 729953 ,58 ,' JAN ',' SKORNIAK ', 715264437133 )*/
+
+
+/*SELECT * FROM zatrudniony*/
+
+
+		
+
+
+
+
+
