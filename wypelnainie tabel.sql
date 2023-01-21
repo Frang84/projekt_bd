fc850a14906303b70c57893f70ed8dcf018dbd81
Frang84 <91824293+Frang84@users.noreply.github.com>
2023-01-21 18:27:09 +0100

    Update wypelnainie tabel.sql

--- wypelnainie tabel.sql
+++ wypelnainie tabel.sql
@@ -14,22 +14,22 @@ VALUES('Biala',2,1,'Lotos', '2015-10-10',5,2,'W','T','chudy kto stal sie gruby',
 
 /*select * from Zwierzeta*/
 
-/*INSERT INTO gatunki (nazwa)
+INSERT INTO gatunki (nazwa)
 VALUES ('pies'),
-		('swinka morska')*/
+('swinka morska')
 
 
-/*INSERT INTO Statusy([nazwa statusu])
+INSERT INTO Statusy([nazwa statusu])
 VALUES ('do adopcji'),
 		('dom tymczasowy'),
 		('w trakcie adopcji'),
 		('pilne'),
 		('wkrotce do adopcji'),
 		('w trakcie leczenia'),
-		('do adopcji z domu tymczasowego')*/
+		('do adopcji z domu tymczasowego')
 
 
-/*INSERT INTO karmy(nazwa,ilosc,Cena,dostawca)
+INSERT INTO karmy(nazwa,ilosc,Cena,dostawca)
 VALUES  ('Favorite', 100, 4.19,'Karmex'),
 		('CEBICAN DAILY CARE DLA PSÓW DOROSŁYCH', 400,6.44,'Karmex'),
 		('SUCHA KARMA JOSERA JOSIDOG ADULT ECONOMY 15', 1500,7.70,'Karmex'),
@@ -39,12 +39,12 @@ VALUES  ('Favorite', 100, 4.19,'Karmex'),
 		('Purizon Adult dla kota, ryba',2000,44.50,'Karmex'),
 		('Purizon Kitten, kurczak i ryba',345,65.33,'Karmex'),
 		('Korzystny pakiet Feringa Classic Meat Menu',543,26.17,'POL-KARM'),
-		('Feringa karma mokra dla kociąt',120,29.0,'POL-KARM')*/
+		('Feringa karma mokra dla kociąt',120,29.0,'POL-KARM')
 
-/*SELECT * FROM karmy*/
+
 	
 
-/*INSERT INTO rasa([ID gatunku],[nazwa rasy])
+INSERT INTO rasa([ID gatunku],[nazwa rasy])
 VALUES (1,'Dachowiec'),
 		(1,'Brytyjski'),
 		(1,'Maine Coon'),
@@ -62,9 +62,9 @@ VALUES (1,'Dachowiec'),
 		(2,'Labrador'),
 		(2,'Pinczer'),
 		(2,'Bokser'),
-		(2,'Jamnik')*/
+		(2,'Jamnik')
 
-/*INSERT INTO zatrudniony
+INSERT INTO zatrudniony
 VALUES (NULL, 132201834 ,43 ,' NADIA ',' MICHALSKI ', 106325829852 ),
 (NULL, 369611698 ,69 ,' PAWEŁ ',' ADAMIUK ', 737414920649 ),     
 (NULL, 183621047 ,28 ,' PIOTR ',' WOJCIECHOWSKI ', 49145971976 ),
@@ -77,10 +77,38 @@ VALUES (NULL, 132201834 ,43 ,' NADIA ',' MICHALSKI ', 106325829852 ),
 (NULL, 744730983 ,41 ,' HANNA ',' JANKOWSKI ', 70387879924 ),
 (NULL, 614392868 ,59 ,' JAN ',' ADAMIEC ', 461852974240 ),
 (NULL, 766700276 ,67 ,' ADRIANNA ',' KWIATKOWSKI ', 819744675271 ),
-(NULL, 512627358 ,58 ,' ANDRZEJ ',' ADAMKIEWICZ ', 927723168654 )*/
+(NULL, 512627358 ,58 ,' ANDRZEJ ',' ADAMKIEWICZ ', 927723168654 )
+
+SET IDENTITY_INSERT dbo.wolontariusze ON
+GO
+SET IDENTITY_INSERT dbo.pracownicy ON
+GO
 
 
-/*SELECT * FROM zatrudniony*/
+INSERT INTO wolontariusze([ID zatrudnionego],[Ilosc godzin])
+VALUES
+(2,0),
+(4,0),
+(5,0),
+(12,0),
+(13,0)
+
+
+
+INSERT INTO pracownicy([ID zatrudnionego],[data przyjecia],pensja)
+VALUES
+(1,'2015-10-17',3500),
+(3,'2015-1-7',3200),
+(6,'2000-10-1',3500),
+(7,'2010-10-17',8500),
+(8,'2005-10-17',6500),
+(9,'2017-10-17',7500),
+(10,'2019-10-17',6500),
+(11,'2019-10-17',4500)
+
+
+
+
 
 
 		
