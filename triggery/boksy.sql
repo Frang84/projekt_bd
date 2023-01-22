f3f2c3f8b2fa21f081d6f628906eeafeba756448
zupa87 <121900203+zupa87@users.noreply.github.com>
2023-01-22 16:12:14 +0100

    Update boksy.sql

--- triggery/boksy.sql
+++ triggery/boksy.sql
@@ -1,15 +1,23 @@
-
-
 GO  
 IF OBJECT_ID(N'boks',N'TR') IS NOT NULL  
     DROP TRIGGER boks  
 GO
 CREATE TRIGGER boks
 ON Zwierzeta AFTER INSERT
-AS
- --select [ID Zwierzaka] from inserted
- select [ID Zwierzaka] FROM inserted
- UPDATE boksy
- SET [ID Zwierzaka] = ( SELECT TOP 1 [ID Zwierzaka] FROM inserted )
- WHERE [ID boksu] = (Select TOP 1 [ID boksu] FROM boksy
-					WHERE [ID Zwierzaka] is null  and [ID gatunku] = (SELECT TOP 1 [ID gatunku] FROM inserted ) )
+AS 
+
+DECLARE @idColumn INT
+
+SELECT @idColumn = MIN( [ID Zwierzaka] ) FROM inserted
+
+WHILE @idColumn IS NOT NULL
+BEGIN
+
+	UPDATE boksy
+	SET [ID Zwierzaka] = @idColumn
+	WHERE [ID boksu] = (Select TOP 1 [ID boksu] FROM boksy
+				WHERE [ID Zwierzaka] is null  and [ID gatunku] = (SELECT TOP 1 [ID gatunku] FROM inserted WHERE [ID Zwierzaka] = @idColumn) )
+
+
+    SELECT @idColumn = MIN( [ID Zwierzaka] ) FROM inserted WHERE [ID Zwierzaka] > @idColumn
+END
