f25b0fff62aad5c322a5ed630a94cbdc6750319d
Frang84 <91824293+Frang84@users.noreply.github.com>
2023-01-24 12:39:52 +0100

    Update szczegolyZwierzaka.sql

--- widoki/szczegolyZwierzaka.sql
+++ widoki/szczegolyZwierzaka.sql
@@ -1,13 +1,12 @@
 CREATE VIEW szczegolyZwierzaka AS
 SELECT
-Zwierzeta.[ID Zwierzaka], Zwierzeta.Imie, gatunki.nazwa, rasa.[nazwa rasy],Zwierzeta.plec ,Zwierzeta.waga, zwierzeta.sterylizacja, karmy.nazwa AS 'Nazwa karmy',
-[specjalne potrzeby].opis AS 'Specjalne potrzeby'
+Zwierzeta.[ID Zwierzaka], Zwierzeta.Imie, gatunki.nazwa, rasa.[nazwa rasy],Zwierzeta.plec ,Zwierzeta.waga, zwierzeta.sterylizacja, Zwierzeta.opis,
+[specjalne potrzeby].opis AS 'Specjalne potrzeby',boksy.[ID boksu]
 FROM Zwierzeta
 LEFT JOIN rasa ON rasa.[ID rasy] = Zwierzeta.[ID rasy]
 LEFT JOIN gatunki ON gatunki.[ID gatunku] = Zwierzeta.[ID gatunku]
-LEFT JOIN karmienie ON karmienie.[ID Zwierzaka] = Zwierzeta.[ID Zwierzaka]
-LEFT JOIN karmy ON karmy.[ID karmy] = karmienie.[ID karmy] --czy to ma sens 
 LEFT JOIN [specjalne potrzeby] ON [specjalne potrzeby].[ID Zwierzaka] = Zwierzeta.[ID Zwierzaka]
+LEFT JOIN boksy ON boksy.[ID Zwierzaka] = Zwierzeta.[ID Zwierzaka]
 
 
 
