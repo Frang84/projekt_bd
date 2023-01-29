8f97442822fc8bd7d3b851117b238ace7ebc2796
Frang84 <91824293+Frang84@users.noreply.github.com>
2023-01-29 22:57:51 +0100

    Add files via upload

---
+++ triggery/dodawanieDaty.sql
@@ -0,0 +1,77 @@
+GO
+IF OBJECT_ID(N'dataPrzyjecia',N'TR') IS NOT NULL  
+    DROP TRIGGER  dataPrzyjecia 
+
+--wyslij 
+/*trigger ktory dodaje czas wypadku gdy osoba wprowadzajaca czas zapomni go dodac. Minus teo rozwiazanie jest to ze trigger nie dziala*/
+GO
+CREATE TRIGGER dataPrzyjecia ON Zwierzeta 
+AFTER INSERT 
+AS 
+	DECLARE @idDodanego INT
+	DECLARE @czasDodania DATE
+	SELECT @idDodanego = MAX([ID Zwierzaka]) FROM Zwierzeta
+	SELECT @czasDodania = [data znalezienia] FROM Zwierzeta
+	WHERE [ID Zwierzaka] = @idDodanego
+	DECLARE @i INT = @@ROWCOUNT
+	WHILE @i >= 0
+	BEGIN
+		IF @czasDodania is NULL 
+			UPDATE Zwierzeta 
+			SET [data znalezienia] = CONVERT(DATE,GETDATE())
+			WHERE [ID Zwierzaka] = (@idDodanego - @i)
+		SET	@i = @i - 1
+		END
+
+--wyslij 
+/*trigger ktory dodaje czzs w wypadku gdyby osoba ktora wpordza dane do systemu zapomniala podac czas znalezienia trigger automatycznie doda obecna chwile.
+Minusem rozwiazania jest to ze dziala tylkow wypadku dodawania pojedynczych wierszy. Dlatego istnieje funkcja dodaj zwierzaka ktora dodaje zwierzeta 
+i pozwala dodawac jedno zwierze na raz.Nie mam kurwa pojecia czy to ma sens*/
+CREATE TRIGGER dataPrzyjecia ON Zwierzeta 
+AFTER INSERT 
+AS 
+	DECLARE @idDodanego INT
+	DECLARE @czasDodania DATE
+	SELECT @idDodanego = MAX([ID Zwierzaka]) FROM Zwierzeta
+	SELECT @czasDodania = [data znalezienia] FROM Zwierzeta
+	WHERE [ID Zwierzaka] = @idDodanego
+		IF @czasDodania is NULL 
+			UPDATE Zwierzeta 
+			SET [data znalezienia] = CONVERT(DATE,GETDATE())
+			WHERE [ID Zwierzaka] = @idDodanego 
+		
+
+
+--wyslij 
+CREATE PROCEDURE dodajZwierzaka 
+(	
+	@imie NVARCHAR(50),
+	@gatunek INT,
+	@rasa INT,
+	@miejsceZnalezienia NVARCHAR(1000),
+	@dataZnalezienia DATE,
+	@wiek INT,
+	@waga INT,
+	@plec NVARCHAR(6),
+	@sterylizacja char(1),
+	@opis NVARCHAR(1000),
+	@zdjecie IMAGE = NULL
+	)
+	AS 
+	BEGIN 
+		INSERT INTO Zwierzeta
+		VALUES 
+		(
+		@imie,
+		@gatunek,
+		@rasa,
+		@miejsceZnalezienia,
+		@dataZnalezienia,
+		@wiek,
+		@waga,
+		@plec,
+		@sterylizacja,
+		@opis,
+		@zdjecie
+		)
+	END
